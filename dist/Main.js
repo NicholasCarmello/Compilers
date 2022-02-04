b5a13062ce29458c1a9b55e66fb1b6c3aa172463
Nicholas Carmello <nick.carmello@yahoo.com>
2022-02-04 18:45:13 -0500

    added tests button and regex outline

--- dist/Main.js
+++ dist/Main.js
@@ -1,6 +1,18 @@
 "use strict";
 //Test function 
-function GetDataFromInput(input) {
-    console.log(input + " it worked");
+function GetDataFromInput() {
+    const Input = document.getElementById("Input");
+    alert(Input.value);
+}
+function tests() {
+    document.getElementById("Output").value = "{}";
+}
+function regex() {
+    //Identifier [a-z][a-z0-9]*
+    //Integer 0|([1-9][0-9]*)
+    //Decimal (0|([1-9][0-9]*)) ‘.’ (0|([1-9][0-9]*))+
+    return "";
+}
+function Output() {
 }
 //# sourceMappingURL=Main.js.map
--- source/Main.ts
+++ source/Main.ts
@@ -1,5 +1,17 @@
 //Test function 
-function GetDataFromInput(input: string){
-    console.log(input + " it worked");
-    
-    }
+function GetDataFromInput(): void {
+    const Input = document.getElementById("Input");
+    alert((<HTMLInputElement>Input).value)
+}
+function tests(): void{
+   (<HTMLInputElement>document.getElementById("Output")).value = "{}";
+}
+function regex():string{
+//Identifier [a-z][a-z0-9]*
+//Integer 0|([1-9][0-9]*)
+//Decimal (0|([1-9][0-9]*)) ‘.’ (0|([1-9][0-9]*))+
+return ""
+}
+function Output():void{
+
+}
