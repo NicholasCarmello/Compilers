f933eda93e133dc4ba8d80f82963dda0cec03429
Nicholas Carmello <nick.carmello@yahoo.com>
2022-02-04 20:34:42 -0500

    fixed putting test into input field

---
+++ source/lexer/lexer.ts
@@ -0,0 +1,7 @@
+function lexGreedyApproach(pgm: string):void{
+    let inString: boolean = false
+    let lexicalOrder =  {'keyword': 1,'id':2,'symbol':3,'digit':4, 'char':5}
+    for (var i = 0; i < pgm.length; i ++){
+        
+    }
+}
