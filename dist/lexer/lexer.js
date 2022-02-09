cb62200e74f3d8a2ed7685e556dd7f86c154d448
nicholascarmello <nick.carmello@yahoo.com>
2022-02-09 00:29:51 -0500

    lexer almost working, implemented some keywords and symbols

--- dist/lexer/lexer.js
+++ dist/lexer/lexer.js
@@ -1,29 +1,34 @@
 function lexGreedyApproach() {
     let input = document.getElementById("Input").value;
     let inString = false;
-    let lexicalOrder = { 'keyword': 1, 'id': 2, 'symbol': 3, 'digit': 4, 'char': 5 };
-    let types = { 'int': 'keyword', 'float': 'keyword', 'boolean': 'keyword' };
-    let char = /a-z/;
-    let num = /0-9/;
-    let newInput = input.toString();
+    let lexicalOrder = { 'keyword': 1, 'id': 2, 'symbol': 3, 'digit': 4, 'char': 5, '': 6 };
+    let grammar = { "int": "keyword", "boolean": "keyword", "}": "keyword", "{": "keyword", 'string': 'keyword', '$': "symbol", '=': 'symbol' };
     let currentCursor = 0;
     let secondCursor = 0;
     //Sliding window technique
     let stopSearchingSymbols = ['$', '}', "{", " "];
     let currentWord = "";
-    let highestKind = "";
+    let longestMatch = "";
     while (input[currentCursor] != "$") {
         currentWord += input[secondCursor];
         secondCursor += 1;
+        if (currentWord in grammar) {
+            longestMatch = currentWord;
+        }
         //Second cursor checks to see if the current value is a value we can stop searching at
         if (stopSearchingSymbols.includes(input[secondCursor]) || input[secondCursor] === undefined || currentWord.length > 5) {
             currentCursor += 1;
             secondCursor = currentCursor;
-            console.log(currentWord);
+            if (longestMatch != " " && longestMatch != '') {
+                console.log("LEXER - " + grammar[longestMatch] + " [ " + longestMatch + " ]");
+            }
             currentWord = "";
+            longestMatch = "";
         }
     }
 }
 function regex() {
+    let char = /a-z/;
+    let num = /0-9/;
 }
 //# sourceMappingURL=lexer.js.map
--- source/lexer/lexer.ts
+++ source/lexer/lexer.ts
@@ -1,34 +1,42 @@
 function lexGreedyApproach():void{
     let input: string = (<HTMLInputElement>document.getElementById("Input")).value;
     let inString: boolean = false
-    let lexicalOrder =  {'keyword': 1,'id':2,'symbol':3,'digit':4, 'char':5}
-    let types = {'int' : 'keyword', 'float':'keyword','boolean': 'keyword'}
-    let char = /a-z/;
-    let num = /0-9/;
-    let newInput = input.toString()
+    let lexicalOrder =  {'keyword': 1,'id':2,'symbol':3,'digit':4, 'char':5,'':6}
+    let grammar = {"int": "keyword","boolean":"keyword","}":"keyword","{":"keyword",'string':'keyword','$':"symbol",'=':'symbol'}
+    
     let currentCursor: number = 0;
     let secondCursor: number = 0;
     //Sliding window technique
     let stopSearchingSymbols = ['$','}',"{", " "]
     let currentWord: string = ""
-    let highestKind: string = ""
+    let longestMatch: string = ""
+    
     while (input[currentCursor] != "$") {
+        
         currentWord += input[secondCursor];
         secondCursor += 1;
+    
+        if (currentWord in grammar ){
+            longestMatch = currentWord;
+        }
 
-        
         //Second cursor checks to see if the current value is a value we can stop searching at
         if (stopSearchingSymbols.includes(input[secondCursor]) ||  input[secondCursor] === undefined || currentWord.length > 5){
             
             currentCursor += 1;
             secondCursor = currentCursor
-            console.log(currentWord)
+            if (longestMatch != " " && longestMatch != ''){
+            console.log("LEXER - "  + grammar[longestMatch] +  " [ "  + longestMatch + " ]")
+            }
             currentWord = ""
+            longestMatch= ""
+            
         }
         
         
     }
 }
 function regex(){
-
+    let char = /a-z/;
+    let num = /0-9/;
 }
