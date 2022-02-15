571aa3b7f3573bad677f0638f7207cc3cb0756ee
nicholascarmello <nick.carmello@yahoo.com>
2022-02-14 19:32:05 -0500

    Added a drop down menu with tests

--- dist/Main.js
+++ dist/Main.js
@@ -7,8 +7,31 @@ function getData() {
      }*/
     this.lexGreedyApproach(input);
 }
-function tests() {
-    document.getElementById("Input").value = '{intaintba=0b=0while(a!=3){print(a)while(b!=3){print(b)b=1+bif(b==2){print("there isno spoon")}}b=0a=1+a}}$';
+function tests(event) {
+    //(<HTMLInputElement>document.getElementById("Input")).value = '{intaintba=0b=0while(a!=3){print(a)while(b!=3){print(b)b=1+bif(b==2){print("there isno spoon")}}b=0a=1+a}}$';
+    var selectedElement = event.target;
+    var value = selectedElement.text;
+    if (value == "Alans Progam 1") {
+        document.getElementById("Input").value = '{intaintba=0b=0while(a!=3){print(a)while(b!=3){print(b)b=1+bif(b==2){print("there isno spoon")}}b=0a=1+a}}$';
+    }
+    if (value == "Alans Progam 2") {
+        document.getElementById("Input").value = '{/* Int Declaration */int aint ba = 0b=0/* While Loop */while (a != 3) {print(a) while (b != 3) {print(b) = 1 + bif (b == 2) {/* Print Statement */print("there is no spoon" /* This will do nothing */ )}}b = 0a = 1+a}';
+    }
+    if (value == "Alans Progam 3") {
+        document.getElementById("Input").value = '{intaintba=0b=0while(a!=3){print(a)while(b!=3){print(b)b=1+bif(b==2){print("there isno spoon")}}b=0a=1+a}}$';
+    }
+    if (value == "String declaration") {
+        document.getElementById("Input").value = '{string f = "hello world"}$';
+    }
+    if (value == "Int declaration") {
+        document.getElementById("Input").value = '{int a = 3}$';
+    }
+    if (value == "Bool declaration") {
+        document.getElementById("Input").value = '{boolean b = false}$';
+    }
+    if (value == "Multiple Programs") {
+        document.getElementById("Input").value = '{}$ \n {{{{{{}}}}}}$ \n {{{{{{}}} /* comments are ignored */ }}}}$ \n{ /* comments are still ignored */ int @}$';
+    }
 }
 function clearOutput() {
     document.getElementById("Output").value = "";
--- source/Main.ts
+++ source/Main.ts
@@ -8,8 +8,37 @@ function getData(){
   }*/
   this.lexGreedyApproach(input)
 }
-function tests(): void {
-  (<HTMLInputElement>document.getElementById("Input")).value = '{intaintba=0b=0while(a!=3){print(a)while(b!=3){print(b)b=1+bif(b==2){print("there isno spoon")}}b=0a=1+a}}$';
+function tests(event:any): void {
+  //(<HTMLInputElement>document.getElementById("Input")).value = '{intaintba=0b=0while(a!=3){print(a)while(b!=3){print(b)b=1+bif(b==2){print("there isno spoon")}}b=0a=1+a}}$';
+  var selectedElement = event.target;
+  var value = selectedElement.text;
+  if (value == "Alans Progam 1"){
+    (<HTMLInputElement>document.getElementById("Input")).value = '{intaintba=0b=0while(a!=3){print(a)while(b!=3){print(b)b=1+bif(b==2){print("there isno spoon")}}b=0a=1+a}}$';
+  }
+  if (value == "Alans Progam 2"){
+    (<HTMLInputElement>document.getElementById("Input")).value = '{/* Int Declaration */int aint ba = 0b=0/* While Loop */while (a != 3) {print(a) while (b != 3) {print(b) = 1 + bif (b == 2) {/* Print Statement */print("there is no spoon" /* This will do nothing */ )}}b = 0a = 1+a}';
+
+  }
+  if (value == "Alans Progam 3"){
+    (<HTMLInputElement>document.getElementById("Input")).value = '{intaintba=0b=0while(a!=3){print(a)while(b!=3){print(b)b=1+bif(b==2){print("there isno spoon")}}b=0a=1+a}}$';
+
+  }
+  if (value == "String declaration"){
+    (<HTMLInputElement>document.getElementById("Input")).value = '{string f = "hello world"}$';
+
+  }
+  if (value == "Int declaration"){
+    (<HTMLInputElement>document.getElementById("Input")).value = '{int a = 3}$';
+  }
+  if (value == "Bool declaration"){
+    (<HTMLInputElement>document.getElementById("Input")).value = '{boolean b = false}$';
+  }
+  if (value == "Multiple Programs"){
+    (<HTMLInputElement>document.getElementById("Input")).value = '{}$ \n {{{{{{}}}}}}$ \n {{{{{{}}} /* comments are ignored */ }}}}$ \n{ /* comments are still ignored */ int @}$';
+  }
+
+
+
 }
 function clearOutput(){
   (<HTMLInputElement>document.getElementById("Output")).value = "";
